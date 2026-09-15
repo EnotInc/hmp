d6cb8d63bc1809ac20cd720a0b15500f68c99043
EMosov <egordevil123@gmail.com>
2026-09-15 14:41:29 +0300

    add basic buildin function to work with files

--- evaluator/buildins.go
+++ evaluator/buildins.go
@@ -8,14 +8,23 @@ import (
 )
 
 var buildins = map[string]*object.BuildIn{
-	"len":   {Fn: _len},
+	"len": {Fn: _len},
+
 	"first": {Fn: _first},
 	"last":  {Fn: _last},
 	"tail":  {Fn: _tail},
 	"push":  {Fn: _push},
 	"pop":   {Fn: _pop},
+
 	"print": {Fn: _print},
 	"exit":  {Fn: _exit},
+
+	"exists": {Fn: _exists},
+	"create": {Fn: _create},
+	"read":   {Fn: _read},
+	"write":  {Fn: _write},
+	"delete": {Fn: _delete},
+	"rename": {Fn: _rename},
 }
 
 func _len(args ...object.Object) object.Object {
@@ -132,6 +141,113 @@ func _exit(args ...object.Object) object.Object {
 		fmt.Print(arg.Inspect())
 	}
 	os.Exit(0)
+
+	return NULL
+}
+
+func _exists(args ...object.Object) object.Object {
+	if len(args) != 1 {
+		return newError("wrong number of agruments. got %d, want 1", len(args))
+	}
+
+	if args[0].Type() != object.STRING_OBJ {
+		return newError("argument to 'exists' must be STRING, got %s", args[0].Type())
+	}
+
+	name := args[0].(*object.String)
+	_, err := os.Stat(name.Value)
+	return nativeBoolToBooleanObj(err == nil)
+}
+
+func _create(args ...object.Object) object.Object {
+	if len(args) != 1 {
+		return newError("wrong number of agruments. got %d, want 1", len(args))
+	}
+
+	if args[0].Type() != object.STRING_OBJ {
+		return newError("argument to 'create' must be STRING, got %s", args[0].Type())
+	}
+
+	name := args[0].(*object.String)
+	f, err := os.Create(name.Value)
+	if err != nil {
+		return newError("unable to create file '%s'. Error: %s", name.Value, err)
+	}
+	defer f.Close()
+
+	return NULL
+}
+
+func _read(args ...object.Object) object.Object {
+	if len(args) != 1 {
+		return newError("wrong number of agruments. got %d, want 1", len(args))
+	}
+
+	if args[0].Type() != object.STRING_OBJ {
+		return newError("argument to 'read' must be STRING, got %s", args[0].Type())
+	}
+
+	name := args[0].(*object.String)
+	data, err := os.ReadFile(name.Value)
+	if err != nil {
+		return newError("unable to create file '%s'. Error: %s", name.Value, err)
+	}
+
+	return &object.String{Value: string(data)}
+}
+
+func _write(args ...object.Object) object.Object {
+	if len(args) != 2 {
+		return newError("wrong number of agruments. got %d, want 2", len(args))
+	}
+
+	if args[0].Type() != object.STRING_OBJ || args[1].Type() != object.STRING_OBJ {
+		return newError("argument to 'write' must be STRING, got %s, %s", args[0].Type(), args[1].Type())
+	}
+
+	name := args[0].(*object.String)
+	data := args[1].(*object.String)
+	err := os.WriteFile(name.Value, []byte(data.Value), 0644)
+	if err != nil {
+		return newError("unable to write file '%s'. Error: %s", name.Value, err)
+	}
+
+	return NULL
+}
+
+func _rename(args ...object.Object) object.Object {
+	if len(args) != 2 {
+		return newError("wrong number of agruments. got %d, want 2", len(args))
+	}
+
+	if args[0].Type() != object.STRING_OBJ || args[1].Type() != object.STRING_OBJ {
+		return newError("argument to 'rename' must be STRING, got %s, %s", args[0].Type(), args[1].Type())
+	}
+
+	old := args[0].(*object.String)
+	new := args[1].(*object.String)
+	err := os.Rename(old.Value, new.Value)
+	if err != nil {
+		return newError("unable to rename file '%s'. Error: %s", old.Value, err)
+	}
+
+	return NULL
+}
+
+func _delete(args ...object.Object) object.Object {
+	if len(args) != 1 {
+		return newError("wrong number of agruments. got %d, want 1", len(args))
+	}
+
+	if args[0].Type() != object.STRING_OBJ {
+		return newError("argument to 'delete' must be STRING, got %s", args[0].Type())
+	}
+
+	name := args[0].(*object.String)
+	err := os.Remove(name.Value)
+	if err != nil {
+		return newError("unable to remove file '%s'. Error: %s", name.Value, err)
+	}
 
 	return NULL
 }
